d2122eee1f39232b16ae8602b377982f266dd593
Leonardo Marini <leonardo.marini@gmail.com>
2022-12-02 14:26:19 +0000

    Added auto var file

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "marini-solutions"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
